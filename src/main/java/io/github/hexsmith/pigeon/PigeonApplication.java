4b9ac66eeca00c01d4de392c8f666324d50e5ca3
yuzhiping4380 <yuzhiping4380@haixue.com>
2019-05-23 18:53:47 +0800

    init

---
+++ src/main/java/io/github/hexsmith/pigeon/PigeonApplication.java
@@ -0,0 +1,13 @@
+package io.github.hexsmith.pigeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigeonApplication.class, args);
+    }
+
+}

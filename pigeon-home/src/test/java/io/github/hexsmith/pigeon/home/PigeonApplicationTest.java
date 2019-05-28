88f537f21fb188e6db989d6749c3ee2b81e453dc
hexsmith <yuzhiping140103@hotmail.com>
2019-05-28 20:04:31 +0800

    项目结构调整

---
+++ pigeon-home/src/test/java/io/github/hexsmith/pigeon/home/PigeonApplicationTest.java
@@ -0,0 +1,18 @@
+package io.github.hexsmith.pigeon.home;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple PigeonApplication.
+ */
+public class PigeonApplicationTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
